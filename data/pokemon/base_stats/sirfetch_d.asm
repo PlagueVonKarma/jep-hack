1d1fdbe8dbf5d42ed42be6548c1909ed25973016
Misty <wazzup5400@gmail.com>
2023-10-22 18:23:30 -0400

    ASM Batch #3
    Up to Toedscreuel!

---
+++ data/pokemon/base_stats/sirfetch_d.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  62, 135,  95,  65,  68,  82
+	;   hp  atk  def  spd  sat  sdf
+
+	db FIGHTING, FIGHTING ; type
+	db 25 ; catch rate
+	db 196 ; base exp
+	db NO_ITEM, STICK ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/sirfetch_d/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_FLYING, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING, CUT, FLY
+	; end
