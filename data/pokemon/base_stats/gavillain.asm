23fdbb841566e761b98c890b6670bb7f49126837
Misty <wazzup5400@gmail.com>
2023-10-24 03:16:02 -0400

    Base Stat Batch 4
    A bunch of base stat ASMs, up to Disturban! Also includes Walking Wake!

---
+++ data/pokemon/base_stats/gavillain.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  80,  80,  75,  95,  90,  75
+	;   hp  atk  def  spd  sat  sdf
+
+	db DRAGON, ELECTRIC ; type
+	db 45 ; catch rate
+	db 167 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 25 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/gavillain/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_MONSTER, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ZAP_CANNON, ROCK_SMASH, HIDDEN_POWER, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, DRAGONBREATH, THUNDER, EARTHQUAKE, RETURN, DIG, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, THUNDERPUNCH, REST, ATTRACT, THIEF, FIRE_PUNCH, FURY_CUTTER, CUT, FLASH, FLAMETHROWER, THUNDERBOLT
+	; end
---
+++ data/pokemon/base_stats/walkingwake.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  99,  83,  91, 109, 125,  83
+	;   hp  atk  def  spd  sat  sdf
+
+	db WATER, DRAGON ; type
+	db 3 ; catch rate
+	db 215 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_UNKNOWN ; gender ratio
+	db 100 ; unknown 1
+	db 80 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/walkingwake/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, CUT, SURF, STRENGTH, WHIRLPOOL, WATERFALL, FLAMETHROWER
+	; end
