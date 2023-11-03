8de60fe1a8f6eb8ae8b53e10ece272515b124e97
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-11-03 23:45:04 +0000

    Add translated Route 49 details
    Used Minamitoku's translation of Nihon Route 1 to add substance to Route 49. Now very accurate. Sioned may be moved inside, given he wasn't originally there.

--- maps/Route49.asm
+++ maps/Route49.asm
@@ -1,3 +1,6 @@
+; Text on this route uses translations from Minamitoku.
+; https://www.youtube.com/watch?v=Pe1vsKRl7_M&ab_channel=Minamitoku
+
 	object_const_def
 	const ROUTE_49_BUG_CATCHER
 
@@ -5,6 +8,47 @@ Route49_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+Route49Sign1:
+	jumptext Route49Sign1Text
+
+Route49Sign1Text:
+	text "ROUTE 49" ; Originally Route 1, but since Johto exists now, we use Route 49.
+	line "SILENT HILLS -"
+	cont "OLD CITY" ; Localisation TBD
+	done
+
+Route49Sign2:
+	jumptext Route49Sign2Text
+
+Route49Sign2Text:
+	text "QUIET CAVE"
+	
+	para "Please be aware"
+	line "of wild #MON!"
+	done
+
+Route49Boy1:
+	jumptextfaceplayer Route49Boy1Text
+
+; Minamitoku stated that the school the boy went to here is used to prepare for exams and the like, so this seems to be the best localisation.
+Route49Boy1Text:
+	text "I saw a strange"
+	line "#MON when I came"
+	cont "back from prep"
+	cont "school!"
+	done
+
+Route49Boy2:
+	jumptextfaceplayer Route49Boy2Text
+
+Route49Boy2Text:
+	text "Hey, pal!" ; "Hey, boy!" originally, localised with gender-neutral pronouns to account for Crystal.
+	para "You should use"
+	line "# BALLs after"
+	cont "you've weakened"
+	cont "the #MON!"
+	done
 
 TrainerBugCatcherSioned:
 	trainer BUG_CATCHER, SIONED, EVENT_BEAT_BUG_CATCHER_SIONED, BugCatcherSionedSeenText, BugCatcherSionedBeatenText, 0, .Script
@@ -42,12 +86,16 @@ Route49_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	;warp_event  8,  9, SILENT_CAVE, 1
-	;warp_event  8,  8, SILENT_CAVE, 1
+	;warp_event  8,  9, QUIET_CAVE, 1
+	;warp_event  8,  8, QUIET_CAVE, 1
 
 	def_coord_events
 
 	def_bg_events
+	bg_event 20,  8, BGEVENT_READ, Route49Sign1
+	bg_event 12,  7, BGEVENT_READ, Route49Sign2
 
 	def_object_events
 	object_event 23,  7, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 5, TrainerBugCatcherSioned, -1
+	object_event 20,  5, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route49Boy2, -1
+	object_event 19, 12, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, Route49Boy1, -1
