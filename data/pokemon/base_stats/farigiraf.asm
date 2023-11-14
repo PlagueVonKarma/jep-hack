a9163e91f4de12d0ddc7ac1b04e15f7d267562b2
Misty <wazzup5400@gmail.com>
2023-11-13 20:46:42 -0500

    ASM Batch 6
    11 new Base Stat files in order to prepare for additions up to Violet City!

---
+++ data/pokemon/base_stats/farigiraf.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db 120,  90,  70,  60, 110,  70
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, PSYCHIC_TYPE ; type
+	db 45 ; catch rate
+	db 255 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/farigiraf/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_GROUND, EGG_INDETERMINATE ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, ROCK_SMASH, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, EARTHQUAKE, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, STRENGTH, THUNDERBOLT
+	; end
