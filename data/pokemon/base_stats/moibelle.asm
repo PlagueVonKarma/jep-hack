2da479b93cc88992b412ba4c9f9680dcbfe19445
Misty <wazzup5400@gmail.com>
2023-11-19 01:21:35 -0500

    The Road To Violet City!

---
+++ data/pokemon/base_stats/moibelle.asm
@@ -0,0 +1,25 @@
+	db 0 ; species ID placeholder
+
+	db  40,  65,  40,  70,  65,  50
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, DARK ; type
+	db 255 ; catch rate
+	db 71 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F100 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/moibelle/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+<<<<<<< HEAD
+	dn EGG_GROUND, EGG_GROUND ; egg groups
+=======
+	dn EGG_FIELD, EGG_FIELD ; egg groups
+>>>>>>> 2a6935c344e316fbd63afd3238dcef0d20f2c7da
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, RETURN, DIG, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, NIGHTMARE, CUT, FLASH, THUNDERBOLT
+	; end
