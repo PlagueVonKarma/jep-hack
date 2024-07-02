8be411a6b646aa240b0236e34d596cb4d046c253
Misty Cass <wazzup5400@gmail.com>
2024-07-02 01:09:42 -0400

    Violet to Ilex - Setup

---
+++ data/pokemon/base_stats/hippunk.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  90,  75,  75,  30,  80,  80
+	;   hp  atk  def  spd  sat  sdf
+
+	db PSYCHIC_TYPE, DARK ; type
+	db 160 ; catch rate
+	db 110 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/hippunk/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_MONSTER, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROLLOUT, ROAR, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, PSYCHIC_M, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DEFENSE_CURL, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, SURF, STRENGTH, FLASH
+	; end
---
+++ data/pokemon/base_stats/reroad.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  90,  85,  75,  70,  85,  70
+	;   hp  atk  def  spd  sat  sdf
+
+	db WATER, WATER ; type
+	db 120 ; catch rate
+	db 130 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/reroad/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_WATER_1, EGG_WATER_1 ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, THUNDERPUNCH, REST, ATTRACT, THIEF, FURY_ATTACK, CUT, SURF, WHIRLPOOL, ICE_BEAM
+	; end
