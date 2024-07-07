356b9183fb9dc92dfa48ea54a691bb4e833b7096
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2024-07-08 00:33:06 +0100

    AA
    no forget this

--- maps/Route36NationalParkGate.asm
+++ maps/Route36NationalParkGate.asm
@@ -247,7 +247,7 @@ Route36Officer_ContestHasConcluded:
 .Sunstone:
 	writetext Route36NationalParkGateOfficer1HeresThePrizeText
 	promptbutton
-	verbosegiveitem SUN_STONE
+	verbosegiveitem BLK_AUGURITE
 	iffalse .BagFull
 	clearevent EVENT_CONTEST_OFFICER_HAS_SUN_STONE
 	closetext
