ba9010487905063e61fcb9103814bc661fa38ad1
U-DESKTOP-IKOGN35\wazzu <wazzup5400@gmail.com>
2024-07-09 00:01:50 -0400

    Some Light Prep

---
+++ data/pokemon/base_stats/bonsly.asm
@@ -0,0 +1,21 @@
+	db BONSLY ; species ID placeholder
+
+	db  50,  80,  95,  10,  10,  45
+	;   hp  atk  def  spd  sat  sdf
+
+	db ROCK, ROCK ; type
+	db 255 ; catch rate
+	db 68 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/bonsly/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, DEFENSE_CURL, REST, ATTRACT, THIEF
+	; end
---
+++ data/pokemon/base_stats/caretorker.asm
@@ -0,0 +1,21 @@
+	db CARETORKER ; species ID placeholder
+
+	db  90,  65,  65,  72,  75, 118
+	;   hp  atk  def  spd  sat  sdf
+
+	db FLYING, FAIRY ; type
+	db 90 ; catch rate
+	db 200 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F100 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/caretorker/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_FLYING, EGG_FLYING ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SWEET_SCENT, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, PSYCHIC_M, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, REST, ATTRACT, STEEL_WING, FLY
+	; end
