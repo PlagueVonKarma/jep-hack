e552a0f152c1dbd085090993eec2acf6cf76964a
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2024-07-17 14:21:11 +0100

    Fix some map spawns
    When you die or fly to one of these places you don't get absolutely fuckin plastered anymore thank me later

--- data/maps/spawn_points.asm
+++ data/maps/spawn_points.asm
@@ -30,16 +30,16 @@ SpawnPoints:
 	spawn ROUTE_32,                   11, 74
 	spawn AZALEA_TOWN,                15, 10
 	spawn CIANWOOD_CITY,              23, 44
-	spawn GOLDENROD_CITY,             15, 28
+	spawn GOLDENROD_CITY,             14, 28
 	spawn OLIVINE_CITY,               13, 22
 	spawn ECRUTEAK_CITY,              23, 28
 	spawn MAHOGANY_TOWN,              15, 14
-	spawn LAKE_OF_RAGE,               21, 29
+	spawn LAKE_OF_RAGE,                5, 20
 	spawn BLACKTHORN_CITY,            21, 30
 	spawn SILVER_CAVE_OUTSIDE,        23, 20
 	spawn FAST_SHIP_CABINS_SW_SSW_NW,  6,  2
 	
-	spawn SILENT_HILLS,				  10, 10
+	spawn SILENT_HILLS,				  13,  5
 	
 	spawn N_A,                        -1, -1
 
