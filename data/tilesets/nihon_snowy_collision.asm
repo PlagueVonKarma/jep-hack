4b9ed6021d25df17ea0dc3cbb4cfe5f5f515673b
Zeta_Null <z0zetanull@gmail.com>
2024-07-17 13:38:03 -0400

    Blue Forest Tweaks

--- data/tilesets/nihon_snowy_collision.asm
+++ data/tilesets/nihon_snowy_collision.asm
@@ -6,8 +6,8 @@
 	tilecoll FLOOR, FLOOR, WALL, WALL ; 05
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 06
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 07
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 08
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 09
+	tilecoll WALL, WALL, WALL, WALL ; 08
+	tilecoll WALL, WALL, WALL, WALL ; 09
 	tilecoll WALL, WALL, WALL, WALL ; 0a
 	tilecoll WALL, WALL, WALL, WALL ; 0b
 	tilecoll WALL, DOOR, FLOOR, FLOOR ; 0c
@@ -15,7 +15,7 @@
 	tilecoll WALL, WALL, WALL, DOOR ; 0e
 	tilecoll WALL, WALL, WALL, WALL ; 0f
 	tilecoll WALL, WALL, WALL, WALL ; 10
-	tilecoll WALL, WALL, WALL, WALL ; 11
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 11
 	tilecoll WALL, WALL, DOOR, DOOR ; 12
 	tilecoll WALL, WALL, WALL, WALL ; 13
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 14
@@ -26,7 +26,7 @@
 	tilecoll WALL, WALL, WALL, WALL ; 19
 	tilecoll WALL, WALL, FLOOR, WALL ; 1a
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 1b
-	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 1c
+	tilecoll FLOOR, WALL, WALL, WALL ; 1c
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1d
 	tilecoll WALL, FLOOR, WALL, WALL ; 1e
 	tilecoll FLOOR, FLOOR, WALL, WALL ; 1f
@@ -35,3 +35,12 @@
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 22
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 23
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 24
+	tilecoll WALL, WALL, WALL, WALL ; 25
+	tilecoll WALL, WALL, WALL, FLOOR ; 26
+	tilecoll WALL, WALL, FLOOR, WALL ; 27
+	tilecoll WALL, WALL, WALL, FLOOR ; 28
+	tilecoll WALL, WALL, FLOOR, WALL ; 29
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 2a
+	tilecoll WATER, WATER, WATER, WATER ; 2b
+	tilecoll WATER, WATER, WATER, WATER ; 2c
+	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 2d
