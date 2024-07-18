b705dbf6ae8f3192cb74a87546a6d2df90654e0e
Martha Schilling <harleyrayquaza@gmail.com>
2024-07-18 16:13:32 +0100

    Caretorker!
    Sprites from Pokemon October, shiny palette by me, and I also gave it a cry that I think works.

--- gfx/pokemon/caretorker/anim.asm
+++ gfx/pokemon/caretorker/anim.asm
@@ -1,1 +1,15 @@
+	frame  0, 08
+	frame  1, 06
+	frame  2, 10
+	frame  0, 06
+	frame  2, 10
+	frame  0, 04
+	frame  3, 08
+	frame  4, 08
+	frame  3, 08
+	frame  4, 08
+	frame  3, 08
+	frame  4, 08
+	frame  3, 06
+	frame  5, 18
 	endanim
