b4686d5fb529d985c76f7a58ad24f7ab1039b36e
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2024-07-26 17:43:07 +0100

    Two more October sprites
    Folage and Trebir!

--- gfx/pokemon/trebir/anim.asm
+++ gfx/pokemon/trebir/anim.asm
@@ -1,1 +1,15 @@
+	frame  0, 18
+	frame  1, 18
+	frame  2, 10
+	frame  3, 20
+	frame  4, 10
+	frame  5, 10
+	frame  6, 10
+	frame  7, 10
+	frame  8, 10
+	frame  9, 10
+	frame 10, 10
+	frame 11, 10
+	frame  1, 10
+	frame 12, 18
 	endanim
