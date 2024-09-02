84c101dcfaba136cb6fc419700f0505ec6f48785
LuciShrimp <gruenrosablau123@googlemail.com>
2024-09-02 17:14:54 +0200

    Small update to CoolCave
    Just pitch shifting the whole thing slightly. I don't know, I just like it better this way.

--- audio/music/CoolCave.asm
+++ audio/music/CoolCave.asm
@@ -1,5 +1,5 @@
 ; Cool Cave theme
-; 4/4 rhythm, composed in G Phrygian.
+; 4/4 rhythm, composed in F Phrygian.
 ; Custom waveforms are required, specifically the 10th one from the JEP wave_samples.asm file.
 ; This theme started out with two ideas. Firstly, I really wanted to try my hand at negative harmony. I initially composed this in G Major and just went and inverted all the intervals. The vibe of the song is something I probably
 ;   would not have cooked up without this plan.
@@ -35,77 +35,76 @@ Music_CoolCave_Ch1:
 	tempo 201
 	duty_cycle 3
 	volume_envelope 12, 7
+	octave 1
+	note A#, 3
+	note A#, 1
 	octave 2
-	note C_, 3
-	note C_, 1
-	note G_, 2
 	note F_, 2
-	note C_, 3
+	note D#, 2
+	octave 1
+	note A#, 3
 	note_type 9, 12, 7
-	note C_, 1
+	note A#, 1
+	octave 2
 	note_type 3, 12, 7
-	note E_, 1
-	note_type 12, 12, 7
-	note F_, 2
-	note D#, 1
 	note D_, 1
-	note C_, 3
-	note C_, 1
-	note F_, 2
-	note D#, 2
-	note D_, 4
-	octave 1
-	note A#, 4
-	octave 2
-	note C_, 3
-	note C_, 1
-	note G_, 2
-	note F_, 2
-	note C_, 3
-	note_type 9, 12, 7
-	note C_, 1
-	note_type 3, 12, 7
-	note A_, 1
 	note_type 12, 12, 7
-	note A#, 2
-	note G#, 1
-	note G_, 1
-	note D_, 3
-	note D_, 1
-	note F_, 2
 	note D#, 2
-	note D_, 4
-	octave 1
-	note A#, 4
-	octave 2
-	note C_, 8
-	octave 1
-	note F_, 8
-	note A#, 8
-	note D#, 8
-	note G_, 4
-	octave 2
-	note C_, 4
-	note C_, 4
-	note C_, 4
-	octave 1
-	note G#, 2
-	octave 2
-	note D#, 2
-	octave 1
-	note G#, 2
-	octave 2
-	note D#, 2
-	octave 1
-	note F_, 1
-	octave 2
+	note C#, 1
 	note C_, 1
 	octave 1
-	note F_, 1
+	note A#, 3
+	note A#, 1
 	octave 2
-	note C_, 1
+	note D#, 2
+	note C#, 2
+	note C_, 4
 	octave 1
 	note G#, 4
+	note A#, 3
+	note A#, 1
+	octave 2
+	note F_, 2
+	note D#, 2
+	octave 1
+	note A#, 3
+	note_type 9, 12, 7
+	note A#, 1
+	octave 2
+	note_type 3, 12, 7
+	note G_, 1
+	note_type 12, 12, 7
+	note G#, 2
+	note F#, 1
+	note F_, 1
+	note C_, 3
+	note C_, 1
+	note D#, 2
+	note C#, 2
+	note C_, 4
+	octave 1
+	note G#, 4
+	note A#, 8
+	note D#, 8
+	note G#, 8
+	note C#, 8
+	note F_, 4
+	note A#, 4
+	note A#, 4
+	note A#, 4
+	note F#, 2
+	octave 2
+	note C#, 2
+	octave 1
+	note F#, 2
+	octave 2
+	note C#, 2
+	octave 1
+	note D#, 1
+	note A#, 1
+	note D#, 1
+	note A#, 1
+	note F#, 4
 	octave 2
 	sound_call .sub1
 	note_type 12, 15, 7
@@ -118,54 +117,50 @@ Music_CoolCave_Ch1:
 	duty_cycle 3
 	octave 1
 	volume_envelope 12, 7
-	note G_, 3
-	note G_, 1
-	octave 2
-	note D_, 2
-	note C_, 2
-	octave 1
-	note G_, 3
-	note_type 9, 12, 7
-	note G_, 1
-	note_type 3, 12, 7
-	note B_, 1
-	note_type 12, 12, 7
+	note F_, 3
+	note F_, 1
 	octave 2
 	note C_, 2
 	octave 1
-	note A#, 1
-	note G#, 1
-	note G_, 3
-	note G_, 1
 	note A#, 2
-	note G#, 2
-	note G_, 3
-	note G_, 1
-	note F_, 4
-	note G_, 3
-	note G_, 1
-	octave 2
-	note D_, 2
-	note C_, 2
-	octave 1
-	note G_, 3
+	note F_, 3
 	note_type 9, 12, 7
-	note G_, 1
+	note F_, 1
 	note_type 3, 12, 7
-	note B_, 1
+	note A_, 1
 	note_type 12, 12, 7
+	note A#, 2
+	note G#, 1
+	note F#, 1
+	note F_, 3
+	note F_, 1
+	note G#, 2
+	note F#, 2
+	note F_, 3
+	note F_, 1
+	note D#, 4
+	note F_, 3
+	note F_, 1
 	octave 2
 	note C_, 2
 	octave 1
-	note A#, 1
-	note G#, 1
-	note G_, 3
-	note G_, 1
 	note A#, 2
+	note F_, 3
+	note_type 9, 12, 7
+	note F_, 1
+	note_type 3, 12, 7
+	note A_, 1
+	note_type 12, 12, 7
+	note A#, 2
+	note G#, 1
+	note F#, 1
+	note F_, 3
+	note F_, 1
 	note G#, 2
-	note G_, 3
-	note G_, 1
-	note G#, 4
+	note F#, 2
+	note F_, 3
+	note F_, 1
+	note F#, 4
 	octave 2
 	sound_ret
 
@@ -175,54 +170,50 @@ Music_CoolCave_Ch1:
 	duty_cycle 3
 	octave 1
 	volume_envelope 12, 7
-	note G_, 3
-	note G_, 1
-	octave 2
-	note D_, 2
-	note C_, 2
-	octave 1
-	note G_, 3
-	note_type 9, 12, 7
-	note G_, 1
-	note_type 3, 12, 7
-	note B_, 1
-	note_type 12, 12, 7
+	note F_, 3
+	note F_, 1
 	octave 2
 	note C_, 2
 	octave 1
-	note A#, 1
-	note G#, 1
-	note G_, 3
-	note G_, 1
 	note A#, 2
-	note G#, 2
-	note G_, 3
-	note G_, 1
-	note F_, 4
-	note G_, 3
-	note G_, 1
-	octave 2
-	note D_, 2
-	note C_, 2
-	octave 1
-	note G_, 3
+	note F_, 3
 	note_type 9, 12, 7
-	note G_, 1
+	note F_, 1
 	note_type 3, 12, 7
-	note B_, 1
+	note A_, 1
 	note_type 12, 12, 7
+	note A#, 2
+	note G#, 1
+	note F#, 1
+	note F_, 3
+	note F_, 1
+	note G#, 2
+	note F#, 2
+	note F_, 3
+	note F_, 1
+	note D#, 4
+	note F_, 3
+	note F_, 1
 	octave 2
 	note C_, 2
 	octave 1
-	note A#, 1
-	note G#, 1
-	note G_, 3
-	note G_, 1
 	note A#, 2
+	note F_, 3
+	note_type 9, 12, 7
+	note F_, 1
+	note_type 3, 12, 7
+	note A_, 1
+	note_type 12, 12, 7
+	note A#, 2
+	note G#, 1
+	note F#, 1
+	note F_, 3
+	note F_, 1
 	note G#, 2
-	note G_, 3
-	note G_, 1
-	note G#, 4
+	note F#, 2
+	note F_, 3
+	note F_, 1
+	note F#, 4
 	octave 2
 	sound_ret
 
@@ -231,78 +222,82 @@ Music_CoolCave_Ch2:
 	duty_cycle 3
 	vibrato 13, 3, 3
 	octave 4
-	note D_, 8
+	note C_, 8
 	volume_envelope 6, 7
-	note D_, 8
+	note C_, 8
 	rest 8
 	volume_envelope 12, 7
-	note D#, 8
-	note D_, 8
+	note C#, 8
+	note C_, 8
 	volume_envelope 6, 7
-	note D_, 8
+	note C_, 8
 	note_type 12, 12, 7
 	rest 8
-	note F_, 8
+	note D#, 8
 	volume_envelope 2, 7
 	octave 8
 .mainLoop:
 	volume_envelope 12, 7
 	octave 4
-	note D_, 4
-	note D#, 4
-	note C_, 8
+	note C_, 4
+	note C#, 4
 	octave 3
-	note G#, 8
-	note G_, 4
+	note A#, 8
+	note F#, 8
+	note F_, 4
+	note D#, 1
 	note F_, 1
-	note G_, 1
+	note F#, 1
 	note G#, 1
-	note A#, 1
 	octave 4
-	note D_, 8
+	note C_, 8
 	volume_envelope 6, 7
-	note D_, 8
+	note C_, 8
 	note_type 12, 12, 7
 	octave 3
-	note G#, 8
-	note A#, 4
+	note F#, 8
+	note G#, 4
+	note F#, 1
 	note G#, 1
 	note A#, 1
 	octave 4
 	note C_, 1
-	note D_, 1
-	note G_, 1
 	note F_, 1
 	note D#, 1
-	note D_, 1
-	note C_, 4
-	note D_, 2
+	note C#, 1
+	note C_, 1
+	octave 3
+	note A#, 4
+	octave 4
 	note C_, 2
 	octave 3
 	note A#, 2
-	note G_, 2
-	note G#, 8
-	note F_, 4
-	note C_, 1
-	note D_, 1
-	note D#, 1
-	note F_, 1
-	note G_, 8
-	note G#, 4
-	octave 4
-	note C_, 1
-	note D_, 1
-	note C_, 1
-	note D_, 1
-	note F_, 4
+	note G#, 2
+	note F_, 2
+	note F#, 8
 	note D#, 4
-	note D_, 2
+	octave 2
+	note A#, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D#, 1
+	note F_, 8
+	note F#, 4
+	note A#, 1
+	octave 4
+	note C_, 1
+	octave 3
+	note A#, 1
+	octave 4
+	note C_, 1
+	note D#, 4
+	note C#, 4
 	note C_, 2
 	octave 3
 	note A#, 2
-	octave 4
-	note C_, 2
-	octave 3
+	note G#, 2
+	note A#, 2
 	sound_call .sub1
 	rest 1
 	octave 3
@@ -322,55 +317,58 @@ Music_CoolCave_Ch2:
 .loop1:
 	volume_envelope 12, 7
 	octave 4
-	note G_, 1
+	note F_, 1
 	rest 1
-	note C_, 1
+	octave 3
+	note A#, 1
+	octave 4
 	rest 1
 	volume_envelope 6, 7
-	note G_, 1
+	note F_, 1
 	rest 1
-	note C_, 1
 	octave 3
+	note A#, 1
 	rest 1
 	sound_loop 2, .loop1
 .loop2:
 	volume_envelope 12, 7
 	octave 4
-	note D#, 1
+	note C#, 1
 	octave 3
-	note G#, 1
+	note F#, 1
 	sound_loop 4, .loop2
 	octave 2
 .loop3:
-	octave 4
-	note C_, 1
 	octave 3
-	note F_, 1
+	note A#, 1
+	note D#, 1
 	octave 2
 	sound_loop 2, .loop3
 	octave 3
+	note G#, 1
+	note A_, 1
 	note A#, 1
 	note B_, 1
 	octave 4
-	note C_, 1
-	note C#, 1
-	note D_, 8
+	note C_, 8
 	volume_envelope 6, 7
-	note D_, 8
+	note C_, 8
 	rest 8
 	volume_envelope 12, 7
-	note D#, 7
-	note C_, 1
-	note D_, 8
+	note C#, 7
+	octave 3
+	note A#, 1
+	octave 4
+	note C_, 8
 	volume_envelope 6, 7
-	note D_, 8
+	note C_, 8
 	note_type 12, 12, 7
-	note F_, 2
 	note D#, 2
-	note D_, 2
-	note D#, 2
+	note C#, 2
+	note C_, 2
+	note C#, 2
+	note D#, 4
 	note F_, 4
-	note G_, 4
 	octave 8
 	note_type 12, 15, 8
 	sound_loop 0, .mainLoop
@@ -378,26 +376,10 @@ Music_CoolCave_Ch2:
 .sub1:
 	volume_envelope 12, 7
 	octave 4
-	note F_, 1
+	note D#, 1
 	rest 1
 	volume_envelope 6, 7
-	note F_, 1
-	rest 1
-	volume_envelope 12, 7
-	note C_, 1
-	rest 1
-	volume_envelope 6, 7
-	note C_, 1
-	octave 2
-	sound_ret
-
-.sub2:
-	volume_envelope 12, 7
-	octave 4
-	note D_, 1
-	rest 1
-	volume_envelope 6, 7
-	note D_, 1
+	note D#, 1
 	rest 1
 	volume_envelope 12, 7
 	octave 3
@@ -405,6 +387,23 @@ Music_CoolCave_Ch2:
 	rest 1
 	volume_envelope 6, 7
 	note A#, 1
+	octave 2
+	sound_ret
+
+.sub2:
+	volume_envelope 12, 7
+	octave 4
+	note C_, 1
+	rest 1
+	volume_envelope 6, 7
+	note C_, 1
+	rest 1
+	volume_envelope 12, 7
+	octave 3
+	note G#, 1
+	rest 1
+	volume_envelope 6, 7
+	note G#, 1
 	octave 2
 	sound_ret
 
@@ -422,134 +421,119 @@ Music_CoolCave_Ch3:
 	vibrato 3, 2, 2
 	octave 5
 	volume_envelope 1, 10
-	note F_, 2
+	note D#, 2
 	octave 4
-	note A#, 1
-	note A#, 1
+	note G#, 1
+	note G#, 1
 	octave 5
 	volume_envelope 2, 10
-	note F_, 2
+	note D#, 2
 	octave 4
-	note A#, 1
-	note A#, 1
+	note G#, 1
+	note G#, 1
 	octave 5
 	volume_envelope 1, 10
-	note D#, 2
+	note C#, 2
 	octave 4
-	note G#, 1
-	note G#, 1
+	note F#, 1
+	note F#, 1
 	octave 5
 	volume_envelope 2, 10
-	note D#, 2
+	note C#, 2
 	octave 4
-	note G#, 1
-	note G#, 1
+	note F#, 1
+	note F#, 1
 	octave 6
 	sound_call .sub2
 	note_type 12, 1, 10
 	octave 5
-	note A#, 2
-	note D#, 1
-	note D#, 1
+	note G#, 2
+	note C#, 1
+	note C#, 1
 	volume_envelope 2, 10
-	note A#, 2
-	note D#, 1
-	note D#, 1
+	note G#, 2
+	note C#, 1
+	note C#, 1
 	volume_envelope 1, 10
-	note G#, 2
-	note D_, 1
-	note D_, 1
+	note F#, 2
+	note C_, 1
+	note C_, 1
 	volume_envelope 2, 10
-	note G#, 2
-	note D_, 1
-	note D_, 1
+	note F#, 2
+	note C_, 1
+	note C_, 1
 	volume_envelope 1, 11
 .loop1:
 	volume_envelope 1, 10
-	octave 6
-	note C_, 2
 	octave 5
-	note F_, 1
-	note F_, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
 	volume_envelope 2, 10
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
 	volume_envelope 3, 10
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
 	octave 6
 	volume_envelope 1, 10
-	note D#, 2
+	note C#, 2
 	octave 5
-	note G#, 1
-	note G#, 1
+	note F#, 1
+	note F#, 1
 	octave 6
 	volume_envelope 2, 10
+	note C#, 2
+	octave 5
+	note F#, 1
+	note F#, 1
+	octave 6
+	volume_envelope 1, 10
 	note D#, 2
 	octave 5
 	note G#, 1
 	note G#, 1
 	octave 6
-	volume_envelope 1, 10
 	note F_, 2
 	octave 5
 	note A#, 1
 	note A#, 1
-	octave 6
-	note G_, 2
-	note C_, 1
-	note C_, 1
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
 	volume_envelope 3, 10
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
 	octave 6
 	volume_envelope 1, 10
-	note D#, 2
+	note C#, 2
 	octave 5
-	note G#, 1
-	note G#, 1
+	note F#, 1
+	note F#, 1
 	octave 6
 	volume_envelope 2, 10
-	note D#, 2
+	note C#, 2
 	octave 5
-	note G#, 1
-	note G#, 1
-	octave 6
-	note C_, 2
-	octave 5
-	note F_, 1
-	note F_, 1
-	note G#, 2
-	note D_, 1
-	note D_, 1
+	note F#, 1
+	note F#, 1
+	note A#, 2
+	note D#, 1
+	note D#, 1
+	note F#, 2
+	note C_, 1
+	note C_, 1
 	sound_loop 2, .loop1
 	sound_call .sub1
 	octave 8
@@ -560,26 +544,72 @@ Music_CoolCave_Ch3:
 
 .sub1:
 	volume_envelope 1, 10
-	note G_, 1
+	note F_, 1
 	rest 1
 	volume_envelope 2, 10
-	note G_, 1
+	note F_, 1
 	rest 1
 	volume_envelope 3, 10
-	note G_, 1
+	note F_, 1
 	rest 1
-	note G_, 1
+	note F_, 1
 	rest 1
 	volume_envelope 1, 10
-	note G_, 1
+	note F_, 1
 	rest 1
 	volume_envelope 2, 10
-	note G_, 1
+	note F_, 1
 	rest 1
 	volume_envelope 3, 10
-	note G_, 1
+	note F_, 1
 	rest 1
-	note G_, 1
+	note F_, 1
+	note_type 12, 3, 3
+	rest 1
+	volume_envelope 1, 10
+	note F#, 1
+	rest 1
+	volume_envelope 2, 10
+	note F#, 1
+	rest 1
+	volume_envelope 3, 10
+	note F#, 1
+	rest 1
+	note F#, 1
+	rest 1
+	volume_envelope 1, 10
+	note D#, 1
+	rest 1
+	volume_envelope 2, 10
+	note D#, 1
+	rest 1
+	volume_envelope 3, 10
+	note D#, 1
+	rest 1
+	note D#, 1
+	rest 1
+	volume_envelope 1, 10
+	note F_, 1
+	rest 1
+	volume_envelope 2, 10
+	note F_, 1
+	rest 1
+	volume_envelope 3, 10
+	note F_, 1
+	rest 1
+	note F_, 1
+	note_type 12, 3, 3
+	rest 1
+	volume_envelope 1, 10
+	note F_, 1
+	rest 1
+	volume_envelope 2, 10
+	note F_, 1
+	rest 1
+	volume_envelope 3, 10
+	note F_, 1
+	rest 1
+	note F_, 1
 	note_type 12, 3, 3
 	rest 1
 	volume_envelope 1, 10
@@ -594,83 +624,46 @@ Music_CoolCave_Ch3:
 	note G#, 1
 	rest 1
 	volume_envelope 1, 10
-	note F_, 1
-	rest 1
-	volume_envelope 2, 10
-	note F_, 1
-	rest 1
-	volume_envelope 3, 10
-	note F_, 1
-	rest 1
-	note F_, 1
-	rest 1
-	volume_envelope 1, 10
-	note G_, 1
-	rest 1
-	volume_envelope 2, 10
-	note G_, 1
-	rest 1
-	volume_envelope 3, 10
-	note G_, 1
-	rest 1
-	note G_, 1
-	note_type 12, 3, 3
-	rest 1
-	volume_envelope 1, 10
-	note G_, 1
-	rest 1
-	volume_envelope 2, 10
-	note G_, 1
-	rest 1
-	volume_envelope 3, 10
-	note G_, 1
-	rest 1
-	note G_, 1
-	note_type 12, 3, 3
-	rest 1
-	volume_envelope 1, 10
+	octave 5
 	note A#, 1
 	rest 1
 	volume_envelope 2, 10
 	note A#, 1
-	rest 1
-	volume_envelope 3, 10
-	note A#, 1
-	rest 1
-	note A#, 1
-	rest 1
-	volume_envelope 1, 10
 	octave 6
-	note C_, 1
-	rest 1
-	volume_envelope 2, 10
-	note C_, 1
 	rest 1
 	volume_envelope 1, 10
-	note D_, 1
+	note C_, 1
 	rest 1
 	volume_envelope 2, 10
-	note D_, 1
+	note C_, 1
 	sound_ret
 
 .sub2:
 	vibrato 3, 2, 2
 	octave 5
 	volume_envelope 1, 10
-	note G_, 2
-	note C_, 1
-	note C_, 1
+	note F_, 2
+	octave 4
+	note A#, 1
+	note A#, 1
+	octave 5
 	volume_envelope 2, 10
-	note G_, 2
-	note C_, 1
-	note C_, 1
+	note F_, 2
+	octave 4
+	note A#, 1
+	note A#, 1
+	octave 5
 	volume_envelope 3, 10
-	note G_, 2
-	note C_, 1
-	note C_, 1
-	note G_, 2
-	note C_, 1
-	note C_, 1
+	note F_, 2
+	octave 4
+	note A#, 1
+	note A#, 1
+	octave 5
+	note F_, 2
+	octave 4
+	note A#, 1
+	note A#, 1
+	octave 5
 	sound_ret
 
 Music_CoolCave_Ch4:
