2d0c535396d573554e579b586c64c871439ce1a3
Martha Schilling <harleyrayquaza@gmail.com>
2024-09-17 21:20:48 +0100

    Mt. Moon: now with the extra minute of music it didn't have in GSC for some reason!

--- audio/music/mtmoon.asm
+++ audio/music/mtmoon.asm
@@ -1,3 +1,7 @@
+; Extended Mt. Moon theme
+; GSC only uses the first 20 seconds of this theme. This is an extension which includes the entire track as heard in RBY, fully brought into the GSC sound style.
+; Extended by LuciShrimp.
+
 Music_MtMoon:
 	channel_count 4
 	channel 1, Music_MtMoon_Ch1
@@ -6,23 +10,370 @@ Music_MtMoon:
 	channel 4, Music_MtMoon_Ch4
 
 Music_MtMoon_Ch1:
-	tempo 208
+	tempo 160
 	volume 7, 7
-	duty_cycle 2
-	pitch_offset 1
+	duty_cycle 3
 	vibrato 8, 1, 4
+Music_MtMoon_branch_7e9fc:
+	note_type 12, 12, 3
+Music_MtMoon_branch_7e9fe:
+	octave 4
+	volume_envelope 0, 3
+	tempo 208
+	note D#, 2
+	volume_envelope 4, 5
+	duty_cycle 2
 	stereo_panning TRUE, FALSE
-	note_type 12, 4, 5
+	note D#, 6
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	note A#, 2
+	note B_, 6
+	octave 4
+	note C#, 8
+	octave 3
+Music_MtMoon_branch_7ea11:
 	rest 2
-	sound_loop 0, Music_MtMoon_Ch2.mainloop
+	octave 4
+	stereo_panning TRUE, FALSE
+	note G#, 6
+	note F#, 6
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note E_, 2
+	note D#, 2
+	note E_, 6
+	note F#, 6
+	note E_, 2
+	octave 3
+Music_MtMoon_branch_7ea18:
+	rest 2
+	octave 4
+	stereo_panning TRUE, FALSE
+	note D#, 6
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	note A#, 2
+	note B_, 6
+	octave 4
+	note C#, 10
+	note G#, 2
+	note G#, 2
+	rest 2
+	note G#, 2
+	note A_, 2
+	note A_, 2
+	note A#, 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	note B_, 2
+	note B_, 2
+	octave 5
+	note E_, 4
+	octave 4
+	note B_, 4
+	octave 5
+	note D#, 4
+	octave 4
+	note A#, 4
+	octave 5
+	note D_, 4
+	octave 4
+	note A_, 4
+	octave 5
+	note C#, 4
+	octave 4
+	note G#, 4
+	octave 5
+	note C_, 4
+	octave 4
+	note G_, 4
+	note B_, 4
+	note F#, 4
+	note A#, 4
+	note F_, 4
+	note A_, 4
+	note E_, 4
+	note B_, 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	note A#, 2
+	note A#, 2
+	note A_, 2
+	note A_, 2
+	rest 2
+	note A_, 2
+	note G#, 2
+Music_MtMoon_branch_7ea3c:
+	rest 2
+	note C#, 8
+	rest 2
+	octave 3
+	note B_, 6
+	note A#, 2
+	note B_, 2
+	octave 4
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 6
+	note G_, 4
+	octave 2
+Music_MtMoon_branch_7ea43:
+	rest 2
+	octave 3
+	note G_, 8
+	rest 2
+	note F_, 6
+	note E_, 2
+	note F_, 2
+	note A_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note G_, 6
+	note F_, 2
+	note G_, 2
+	note A_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note G_, 6
+	note A_, 6
+	octave 4
+	note C#, 6
+	note G_, 8
+	rest 2
+	note F_, 6
+	note E_, 2
+	note F_, 2
+	note A_, 2
+	octave 5
+	note C#, 2
+	octave 4
+	note A_, 2
+	note G_, 6
+	note A_, 2
+	octave 5
+	note C#, 2
+	octave 4
+	note A_, 2
+	note G_, 6
+	note A_, 6
+	octave 5
+	note C#, 6
+	octave 4
+	note F#, 8
+	rest 2
+	note E_, 6
+	note D#, 2
+	note E_, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note G#, 6
+	octave 5
+	note C_, 6
+	octave 3
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 2
+	note E_, 2
+	note G#, 2
+	octave 3
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	octave 4
+.loop1:
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	sound_loop 6, .loop1
+	note C_, 2
+	note D#, 2
+	octave 2
+	sound_call Music_MtMoon_branch_7eafc.sub1
+	octave 3
+	note D#, 2
+	octave 2
+	sound_call Music_MtMoon_branch_7eafc.sub1
+	octave 3
+	note_type 12, 4, 5
+	stereo_panning TRUE, FALSE
+	sound_loop 0, Music_MtMoon_branch_7e9fc
+
+Music_MtMoon_branch_7eab2:
+	rest 16
+	octave 3
+	sound_ret
+
+Music_MtMoon_branch_7eac1:
+	rest 16
+	octave 3
+	sound_ret
+
+Music_MtMoon_branch_7eace:
+	stereo_panning TRUE, FALSE
+	rest 16
+	octave 2
+	sound_ret
+
+Music_MtMoon_branch_7eadd:
+	stereo_panning TRUE, FALSE
+	rest 16
+	octave 2
+	sound_ret
+
+Music_MtMoon_branch_7eaea:
+	octave 3
+	stereo_panning TRUE, FALSE
+	note G#, 2
+	note E_, 2
+	note G#, 2
+	note E_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note G#, 2
+	note E_, 2
+	octave 4
+	note C_, 2
+	sound_ret
+
+Music_MtMoon_branch_7eaf7:
+	stereo_panning TRUE, FALSE
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	sound_ret
+
+Music_MtMoon_branch_7eafc:
+	tempo 168
+	rest 8
+	tempo 176
+	rest 8
+	tempo 184
+	rest 8
+	tempo 192
+	rest 8
+	tempo 200
+	rest 8
+	tempo 208
+	rest 8
+	tempo 216
+	rest 8
+	tempo 224
+	rest 8
+	octave 2
+	tempo 160
+	sound_ret
+
+.sub1:
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	sound_ret
 
 Music_MtMoon_Ch2:
 	vibrato 11, 1, 5
-	duty_cycle 2
+	duty_cycle 3
+Music_MtMoon_branch_7eb6d:
 	note_type 12, 8, 4
-	stereo_panning FALSE, TRUE
-.mainloop:
 	octave 4
+	duty_cycle 2
+	stereo_panning FALSE, TRUE
 	note D#, 6
 	note C#, 6
 	octave 3
@@ -70,13 +421,244 @@ Music_MtMoon_Ch2:
 	note E_, 6
 	note F#, 6
 	note E_, 4
-	sound_loop 0, .mainloop
+	note D#, 6
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	note A#, 2
+	note B_, 6
+	octave 4
+	note C#, 8
+	rest 2
+	note G#, 2
+	note G#, 2
+	rest 2
+	note G#, 2
+	note A_, 2
+	note A_, 2
+	note A#, 2
+	note A#, 2
+	rest 2
+	note A#, 2
+	note B_, 2
+	note B_, 2
+	octave 5
+	note E_, 4
+	octave 4
+	note B_, 4
+	octave 5
+	note D#, 4
+	octave 4
+	note A#, 4
+	octave 5
+	note D_, 4
+	octave 4
+	note A_, 4
+	octave 5
+	note C#, 4
+	octave 4
+	note G#, 4
+	octave 5
+	note C_, 4
+	octave 4
+	note G_, 4
+	note B_, 4
+	note F#, 4
+	note A#, 4
+	note F_, 4
+	note A_, 4
+	note E_, 4
+	note B_, 2
+	note B_, 2
+	rest 2
+	note B_, 2
+	note A#, 2
+	note A#, 2
+	note A_, 2
+	note A_, 2
+	rest 2
+	note A_, 2
+	note G#, 2
+	note G#, 2
+	note C#, 8
+	rest 2
+	octave 3
+	note B_, 6
+	note A#, 2
+	note B_, 2
+	octave 4
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 3
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 6
+	note G_, 6
+	octave 3
+	note G_, 8
+	rest 2
+	note F_, 6
+	note E_, 2
+	note F_, 2
+	note A_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note G_, 6
+	note F_, 2
+	note G_, 2
+	note A_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note G_, 6
+	note A_, 6
+	octave 4
+	note C#, 6
+	note G_, 8
+	rest 2
+	note F_, 6
+	note E_, 2
+	note F_, 2
+	note A_, 2
+	octave 5
+	note C#, 2
+	octave 4
+	note A_, 2
+	note G_, 6
+	note A_, 2
+	octave 5
+	note C#, 2
+	octave 4
+	note A_, 2
+	note G_, 6
+	note A_, 6
+	octave 5
+	note C#, 6
+	octave 4
+	note F#, 8
+	rest 2
+	note E_, 6
+	note D#, 2
+	note E_, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note E_, 2
+	note F#, 2
+	note G#, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note G#, 2
+	note F#, 6
+	note G#, 6
+	octave 5
+	note C_, 6
+	octave 3
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	octave 2
+	note E_, 2
+	note G#, 2
+	octave 3
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	octave 4
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note E_, 2
+	note G#, 2
+	note C_, 2
+	note D#, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	sound_loop 0, Music_MtMoon_branch_7eb6d
 
 Music_MtMoon_Ch3:
-.mainloop:
-	note_type 12, 2, 8
+	note_type 12, 1, 2
+	octave 2
 .loop1:
-	octave 2
+	volume_envelope 2, 8
 	note B_, 2
 	octave 3
 	note D#, 2
@@ -90,8 +672,9 @@ Music_MtMoon_Ch3:
 	note B_, 2
 	octave 3
 	note D#, 2
+	octave 2
 	sound_loop 3, .loop1
-	octave 2
+	volume_envelope 2, 8
 	note B_, 2
 	octave 3
 	note D#, 2
@@ -103,7 +686,20 @@ Music_MtMoon_Ch3:
 	note G_, 2
 	note D#, 2
 	note E_, 2
+	note E_, 2
+	note G#, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note E_, 2
+	note G#, 2
+	octave 4
+	note C_, 2
+	octave 3
 .loop2:
+	volume_envelope 2, 8
+	note E_, 2
+	note G#, 2
 	note E_, 2
 	note G#, 2
 	octave 4
@@ -114,26 +710,285 @@ Music_MtMoon_Ch3:
 	octave 4
 	note C_, 2
 	octave 3
+	sound_loop 3, .loop2
+	octave 3
+	volume_envelope 2, 8
 	note E_, 2
 	note G#, 2
-	sound_loop 4, .loop2
-	sound_loop 0, .mainloop
+	octave 2
+.loop3:
+	volume_envelope 2, 8
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	note G_, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note D#, 2
+	octave 2
+	sound_loop 4, .loop3
+	octave 4
+	note_type 12, 2, 8
+	note F_, 2
+	note F_, 2
+	note_type 6, 1, 0
+	rest 4
+	note_type 12, 2, 8
+	note F_, 2
+	note E_, 2
+	note E_, 2
+	note D#, 2
+	note D#, 2
+	note_type 6, 1, 0
+	rest 4
+	note_type 12, 2, 8
+	note D#, 2
+	note D_, 2
+	note D_, 2
+	octave 1
+.loop4:
+	volume_envelope 2, 8
+	octave 2
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	octave 1
+	sound_loop 3, .loop4
+	note_type 12, 2, 8
+	octave 2
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	octave 2
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	octave 2
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	octave 2
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	note C#, 1
+	octave 2
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	octave 4
+	note_type 12, 1, 0
+	note D_, 2
+	note D_, 2
+	note_type 6, 1, 0
+	rest 4
+	note_type 12, 1, 0
+	note D_, 2
+	note D#, 2
+	note D#, 2
+	note E_, 2
+	note E_, 2
+	note_type 6, 1, 0
+	rest 4
+	note_type 12, 1, 0
+	note E_, 2
+	note F_, 2
+	note F_, 2
+	octave 3
+.loop5:
+	note D#, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note G_, 2
+	note D#, 2
+	octave 2
+	note B_, 2
+	octave 3
+	note G_, 2
+	note D#, 2
+	octave 2
+	note B_, 2
+	octave 3
+	sound_loop 4, .loop5
+	octave 2
+.loop6:
+	note A_, 2
+	note F_, 2
+	octave 3
+	sound_call .sub1
+	sound_loop 3, .loop6
+	octave 3
+	sound_call .sub1
+	octave 3
+	note_type 12, 1, 0
+	note A_, 2
+	note F_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note F_, 2
+	octave 4
+	note C#, 2
+	octave 3
+	note A_, 2
+	note F_, 2
+	note A_, 2
+	note F_, 2
+	octave 4
+.loop7:
+	note C#, 2
+	octave 3
+	note A_, 2
+	note F_, 2
+	octave 4
+	sound_loop 6, .loop7
+	octave 3
+.loop8:
+	note G#, 2
+	note E_, 2
+	octave 4
+	sound_call .sub2
+	sound_loop 3, .loop8
+	octave 4
+	sound_call .sub2
+	octave 3
+	note_type 12, 1, 0
+	rest 16
+	rest 8
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	note G_, 2
+	note D#, 2
+	note C#, 6
+	note D#, 2
+	rest 16
+	rest 16
+	sound_loop 0, Music_MtMoon_Ch3
+
+.sub1:
+	note C#, 2
+	octave 2
+	note A_, 2
+	note F_, 2
+	octave 3
+	note C#, 2
+	octave 2
+	note A_, 2
+	note F_, 2
+	sound_ret
+
+.sub2:
+	note C_, 2
+	octave 3
+	note G#, 2
+	note E_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note G#, 2
+	note E_, 2
+	sound_ret
 
 Music_MtMoon_Ch4:
-	toggle_noise 5
+	toggle_noise 0
 	drum_speed 12
-.mainloop:
 	stereo_panning FALSE, TRUE
+	toggle_noise
+	toggle_noise 5
+.loop1:
 	drum_note 11, 4
 	drum_note 11, 8
 	drum_note 11, 4
-	stereo_panning TRUE, FALSE
 	drum_note 11, 4
 	drum_note 11, 4
 	drum_note 11, 4
-	stereo_panning FALSE, TRUE
 	drum_note 11, 4
+	sound_loop 5, .loop1
+	sound_call .sub1
+.loop3:
 	drum_note 11, 4
-	stereo_panning TRUE, FALSE
+	drum_note 11, 6
+	drum_note 11, 6
+	sound_loop 7, .loop3
+.loop2:
+	drum_note 11, 4
 	drum_note 11, 8
-	sound_loop 0, .mainloop
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	sound_loop 10, .loop2
+	octave 8
+	drum_speed 12
+	drum_note 11, 4
+	drum_note 11, 8
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 2
+	sound_ret
+
+.sub1:
+	drum_note 11, 4
+	drum_note 11, 8
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	drum_note 11, 4
+	sound_ret
