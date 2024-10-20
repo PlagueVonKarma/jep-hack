3b26c941bf060c0f4c5adf1f57739f57b155741e
Zeta_Null <z0zetanull@gmail.com>
2024-10-20 14:27:11 -0400

    Silent Hills Audio Tweak
    Unsure if this is good enough to be final, and if someone else wants to touch it up go ahead, I just wanted to make it a bit less. Pitchy.

--- audio/music/nihontest.asm
+++ audio/music/nihontest.asm
@@ -12,87 +12,85 @@ Music_NihonTown01_Ch1:
 	stereo_panning TRUE, FALSE
 .mainLoop:
 	duty_cycle 0
-	tempo 107
 	octave 3
-	volume_envelope 13, 8
 	transpose 0, 0
-	note D#, 4
+	tempo 113
+	volume_envelope 15, 8
+	note E_, 4
 	rest 4
-	note F#, 4
+	note G_, 4
 	rest 4
-	note D#, 4
+	note E_, 4
 	rest 4
-	note C#, 4
+	note D_, 4
 	rest 4
-	note D#, 4
+	note E_, 4
 	rest 4
-	note F#, 4
+	note G_, 4
 	rest 4
-	note C#, 4
-	rest 4
-	octave 2
-	note A#, 4
-	octave 3
-	rest 4
-	note C#, 4
-	rest 4
-	note F_, 4
-	rest 4
-	note D#, 4
-	rest 4
-	note G#, 4
+	note D_, 4
 	rest 4
 	octave 2
 	note B_, 4
 	octave 3
 	rest 4
-	note D#, 4
+	note D_, 4
 	rest 4
-	note C#, 4
+	note F#, 4
 	rest 4
+	note E_, 4
+	rest 4
+	note A_, 4
+	rest 4
+	note C_, 4
+	rest 4
+	note E_, 4
+	rest 4
+	note D_, 4
+	rest 4
+	note C_, 4
 	octave 2
-	note B_, 4
 	rest 4
 .loop1:
 	octave 2
-	note A#, 4
+	note B_, 4
 	rest 4
+	octave 3
+	note C_, 4
+	rest 4
+	note D_, 4
+	rest 4
+	note E_, 4
+	rest 4
+	note E_, 4
+	rest 4
+	note A_, 4
+	rest 4
+	note G_, 4
+	rest 4
+	note D_, 4
+	rest 4
+	note E_, 4
+	rest 4
+	octave 2
+	note A_, 4
+	octave 3
+	rest 4
+	note D_, 4
+	rest 4
+	octave 2
 	note B_, 4
 	octave 3
 	rest 4
-	note C#, 4
+	note D_, 4
 	rest 4
-	note D#, 4
-	rest 4
-	note D#, 4
-	rest 4
-	note G#, 4
-	rest 4
-	note F#, 4
-	rest 4
-	note C#, 4
-	rest 4
-	note D#, 4
+	note E_, 4
 	rest 4
 	octave 2
-	note G#, 4
+	note B_, 4
 	octave 3
 	rest 4
-	note C#, 4
-	rest 4
-	octave 2
-	note A#, 4
-	octave 3
-	rest 4
-	note C#, 4
-	rest 4
-	note D#, 4
-	rest 4
-	octave 2
-	note A#, 4
-	octave 3
-	rest 4
-	note C#, 4
+	note D_, 4
 	rest 4
 	octave 8
 	sound_loop 2, .loop1
@@ -105,84 +103,85 @@ Music_NihonTown01_Ch2:
 	transpose 1, 0
 	duty_cycle 2
 	octave 5
-	volume_envelope 10, 8
-	note F_, 4
+	volume_envelope 12, 8
+	note F#, 4
 	octave 4
-	note F#, 4
-	note F#, 4
-	note F#, 4
+	note G_, 4
+	note G_, 4
+	note G_, 4
 	octave 5
-	note C#, 4
+	note D_, 4
 	octave 4
-	note C#, 4
-	note C#, 4
-	note C#, 4
+	note D_, 4
+	note D_, 4
+	note D_, 4
 	octave 5
-	note D#, 4
+	note E_, 4
 	octave 4
-	note D#, 4
-	note D#, 4
-	note D#, 4
-	note A#, 4
+	note E_, 4
+	note E_, 4
+	note E_, 4
+	note B_, 4
 	octave 3
-	note A#, 4
-	note A#, 4
-	note A#, 4
+	note B_, 4
+	note B_, 4
+	note B_, 4
 	octave 4
-	note A#, 4
-	note C#, 4
-	note C#, 4
-	note C#, 4
-	note G#, 4
+	note B_, 4
+	note D_, 4
+	note D_, 4
+	note D_, 4
+	note A_, 4
 	octave 3
-	note G#, 4
-	note G#, 4
-	note G#, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
 	octave 4
-	note G#, 4
-	note D#, 4
-	note D#, 4
-	note D#, 4
-	note F#, 4
+	note A_, 4
+	note E_, 4
+	note E_, 4
+	note E_, 4
+	note G_, 4
 	octave 3
-	note F#, 4
-	note F#, 4
-	note F#, 4
+	note G_, 4
+	note G_, 4
+	note G_, 4
 .loop1:
 	rest 4
 	octave 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
+	volume_envelope 12, 8
+	note B_, 4
+	note B_, 4
+	note B_, 4
 	rest 4
-	note G#, 4
-	note G#, 4
-	note G#, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
 	rest 4
-	note G#, 4
-	note G#, 4
-	note G#, 4
+	note A_, 4
+	note A_, 4
+	note A_, 4
 	rest 4
-	note F_, 4
-	note F_, 4
-	note F_, 4
+	note F#, 4
+	note F#, 4
+	note F#, 4
 	rest 4
-	note D#, 4
-	note D#, 4
-	note D#, 4
+	note E_, 4
+	note E_, 4
+	note E_, 4
 	rest 4
-	note F_, 4
-	note F_, 4
-	note F_, 4
+	note F#, 4
+	note F#, 4
+	note F#, 4
 	rest 4
-	note A#, 4
-	note A#, 4
-	note A#, 4
+	note B_, 4
+	note B_, 4
+	note B_, 4
 	rest 4
 	octave 3
-	note A#, 4
-	note A#, 4
-	note A#, 4
+	note B_, 4
+	note B_, 4
+	note B_, 4
 	octave 8
 	sound_loop 2, .loop1
 	sound_loop 0, .mainLoop
@@ -190,73 +189,73 @@ Music_NihonTown01_Ch2:
 Music_NihonTown01_Ch3:
 	note_type 12, 1, 0
 .mainLoop:
-	note_type 12, 1, 0
-	volume_envelope 1, 3
-	octave 5
+	note_type 12, 1, 2
 	vibrato 7, 1, 9
-	note G#, 2
-	note A#, 2
-	note A#, 1
-	note B_, 7
-	note G#, 8
-	note F_, 12
-	note F#, 2
-	note G#, 2
-	note G#, 1
-	note A#, 1
-	rest 2
-	note A#, 2
-	rest 2
-	note F#, 2
-	rest 2
-	note F#, 2
-	rest 2
-	note C#, 12
-	note D#, 2
-	note F#, 2
-	note F#, 1
-	note G#, 7
-	note F_, 8
-	note C#, 4
-	note F#, 1
-	note G#, 7
-	note F_, 4
-	vibrato 5, 1, 9
-	note F#, 14
-	vibrato 0, 1, 9
-	note F#, 14
-.loop1:
-	octave 5
-	vibrato 7, 1, 9
-	note D#, 4
-	note F_, 1
-	note F#, 11
-	note F_, 4
-	note C#, 12
 	octave 4
-	note B_, 4
-	octave 5
-	note D#, 1
-	note F_, 3
-	note D#, 4
-	note F#, 4
-	octave 4
-	note A#, 4
+	note A_, 2
+	note B_, 2
 	note B_, 1
 	octave 5
-	note C#, 11
+	note C_, 7
 	octave 4
-	note G#, 4
-	note A#, 1
-	note B_, 11
-	octave 5
-	note D#, 4
-	note C#, 12
-	note F_, 4
-	note F_, 1
-	note F#, 11
+	note A_, 8
+	note F#, 12
+	note G_, 2
+	note A_, 2
+	note A_, 1
+	note B_, 1
+	rest 2
+	note B_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note D_, 12
+	note E_, 2
+	note G_, 2
+	note G_, 1
+	note A_, 7
+	note F#, 8
+	note D_, 4
+	note G_, 1
+	note A_, 7
+	note F#, 4
+	vibrato 5, 1, 9
+	note G_, 14
 	vibrato 0, 1, 9
-	note F#, 16
+	note G_, 14
+	octave 5
+.loop1:
+	vibrato 7, 1, 9
+	octave 4
+	note E_, 4
+	note F#, 1
+	note G_, 11
+	note F#, 4
+	note D_, 12
+	note C_, 4
+	note E_, 1
+	note F#, 3
+	note E_, 4
+	note G_, 4
+	octave 3
+	note B_, 4
+	octave 4
+	note C_, 1
+	note D_, 11
+	octave 3
+	note A_, 4
+	note B_, 1
+	octave 4
+	note C_, 11
+	note E_, 4
+	note D_, 12
+	note F#, 4
+	note F#, 1
+	note G_, 11
+	vibrato 0, 1, 9
+	note G_, 16
 	octave 8
 	sound_loop 2, .loop1
 	sound_loop 0, .mainLoop
@@ -333,4 +332,3 @@ Music_NihonTown01_Ch4:
 	octave 8
 	rest 3
 	sound_loop 0, .loop1
-	sound_loop 0, .mainLoop
