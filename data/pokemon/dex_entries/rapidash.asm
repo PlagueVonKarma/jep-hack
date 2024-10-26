58f5de27fbfdda54bed7bff78ca0ab652f80e376
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2024-10-26 14:41:39 +0100

    New dex entries using DP myth docs
    Piloswine and Lapras pending; seem weak.
    Octillery story seems to lewd even for a game that has decensored much of the original content.
    
    Rapidash and Tauros entries give credence to featuring them around Ruddy Town with Coatl and Xatu.
    
    Typhlosion entry allows for connections to the mujina (even if not wholly intentional, seemingly), which I quite enjoy. However, the dex entry is unsatisfactory.

--- data/pokemon/dex_entries/rapidash.asm
+++ data/pokemon/dex_entries/rapidash.asm
@@ -1,10 +1,15 @@
 	db "FIRE HORSE@" ; species name
 	dw 507, 2090 ; height, weight
 
-	db   "It just loves to"
-	next "gallop. The faster"
-	next "it goes, the long-"
+	db   "Native American"
+	next "myth tells of a"
+	next "greedy hunter wea-"
 
-	page "er the swaying"
-	next "flames of its mane"
-	next "will become.@"
+	page "ring the mane of"
+	next "RAPIDASH, and bei-"
+	next "ng shot by mistake.@"
+; Lore taken from the Pokemon DP design documents.
+; It doesn't seem to be based on any particular myth, but general philosophy.
+; TLDR a man of the east respects the Rapidash, a man of the west does not, and wears their pelt, only to end up being mistaken for one, and hunted down. 
+
+; https://tcrf.net/Development:Pok%C3%A9mon_Diamond_and_Pearl/Documents
--- data/pokemon/dex_entries/typhlosion.asm
+++ data/pokemon/dex_entries/typhlosion.asm
@@ -1,10 +1,19 @@
 	db "VOLCANO@" ; species name
 	dw 507, 1750 ; height, weight
 
-	db   "When heat from its"
-	next "body causes the"
-	next "air around it to"
+	db   "Folklore tells of"
+	next "a shapeshifting"
+	next "TYPHLOSION that"
 
-	page "shimmer, this is a"
-	next "sign that it is"
-	next "ready to battle.@"
+	page "offered a young"
+	next "girl berries and"
+	next "taught her a song.@"
+
+; Extremely abridged version of the infamous Typhlosion story from the DP development docs.
+; Based on the Bear Mother story from Canadian First Nations mythology. I chose to blend it with the mythology of the very similar Mujina (貉).
+; I chose Typhlosion over Hisuian Typhlosion to be consistent with the time period in which the lore was devised.
+; However, as a dex entry, it's quite poor. I think this could be modified and tweaked.
+
+; https://en.wikipedia.org/wiki/Mujina
+; https://nativecanadianarts.com/gallery/the-bear-mother/
+; https://tcrf.net/Development:Pok%C3%A9mon_Diamond_and_Pearl/Documents
