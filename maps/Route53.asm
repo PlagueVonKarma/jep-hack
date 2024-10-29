53fe16d8a97047006bf3cce07278c2163c18a34d
Zeta_Null <z0zetanull@gmail.com>
2024-10-29 00:59:20 -0400

    I need to stop forgetting to include some of the map files lol

---
+++ maps/Route53.asm
@@ -0,0 +1,18 @@
+	object_const_def
+
+Route53_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+Route53_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+
