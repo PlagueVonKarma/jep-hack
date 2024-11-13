5e703191ce9509dff9ff22931adfdbb33db99c65
Zeta_Null <z0zetanull@gmail.com>
2024-11-13 13:46:38 -0500

    More White City Stuff

--- data/tilesets/nihon_whitecity_collision.asm
+++ data/tilesets/nihon_whitecity_collision.asm
@@ -118,3 +118,5 @@
 	tilecoll WALL, WALL, WALL, WALL ; 75
 	tilecoll WALL, WALL, WALL, WALL ; 76
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 77
+	tilecoll WALL, WALL, WALL, WALL ; 78
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 79
