16256d1132cc55fe51ee2318dc654ac315c0af2c
zuperZACH <49823267+zuperZACH@users.noreply.github.com>
2024-11-26 11:02:09 -0700

    Avalop! (#48)
    Adds Avalop's sprites, animations, and shiny

--- gfx/pokemon/mimear/anim.asm
+++ gfx/pokemon/mimear/anim.asm
@@ -1,1 +1,20 @@
+	frame 0, 08
+	frame 1, 10
+	setrepeat 3
+	frame 2, 06
+	frame 1, 06
+	dorepeat 3
+	frame 0, 04
+	frame 4, 08
+	frame 0, 02
+	frame 5, 04
+	frame 0, 02
+	frame 4, 01
+	frame 0, 02
+	setrepeat 2
+	frame 6, 06
+	frame 7, 06
+	dorepeat 14
+	frame 6, 02
+	frame 0, 01
 	endanim
