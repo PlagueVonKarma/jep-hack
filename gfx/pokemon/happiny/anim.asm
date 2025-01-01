227961a3ba7027686c6ae26eaf83b6873ec62782
zuperZACH <zachnewman088@gmail.com>
2025-01-01 13:56:35 -0700

    bapy...
    -Happiny sprites and animations

--- gfx/pokemon/happiny/anim.asm
+++ gfx/pokemon/happiny/anim.asm
@@ -1,1 +1,19 @@
+	frame 0, 08
+	frame 1, 10
+	frame 2, 01
+	frame 3, 01
+	frame 4, 01
+	frame 5, 01
+	setrepeat 3
+	frame 6, 02
+	frame 7, 02
+	frame 8, 02
+	frame 9, 02
+	dorepeat 7
+	frame 5, 01
+	frame 4, 01
+	frame 3, 01
+	frame 2, 01
+	frame 1, 03
+	frame 0, 01
 	endanim
--- gfx/pokemon/happiny/anim_idle.asm
+++ gfx/pokemon/happiny/anim_idle.asm
@@ -1,1 +1,11 @@
+	frame 10, 07
+	frame 11, 04
+	frame 10, 04
+	frame 12, 04
+	frame 10, 04
+	frame 11, 04
+	frame 10, 04
+	frame 12, 04
+	frame 10, 04
+	frame 0,  01
 	endanim
