c3f9e786fe7cfaf91d92706c0ed1d8f30cbbb519
Martha Schilling <harleyrayquaza@gmail.com>
2025-01-13 23:35:51 +0000

    Update dokuroar.asm
    Dex entry!

--- data/pokemon/dex_entries/dokuroar.asm
+++ data/pokemon/dex_entries/dokuroar.asm
@@ -1,10 +1,10 @@
 	db "SKELETAL@" ; species name
 	dw 902, 4960 ; height, weight - placeholder
 
-	db   "Believed to be a"
-	next "spirit inhabiting"
-	next "a super-ancient"
+	db   "The skeleton of a"
+	next "dinosaur given new"
+	next "life. It harbors"
 
-	page "#MON skeleton,"
-	next "but research is"
-	next "still needed.@"
+	page "intense malice due"
+	next "to being forgotten"
+	next "for so long.@"
