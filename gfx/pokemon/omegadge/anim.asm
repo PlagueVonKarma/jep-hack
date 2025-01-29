588aa68db92bd0b175e6a67ec9f52db80ea59968
Zeta_Null <z0zetanull@gmail.com>
2025-01-28 21:04:13 -0500

    Omegadge Graphics (animation may be paced wrong, unimplemented)

---
+++ gfx/pokemon/omegadge/anim.asm
@@ -0,0 +1,15 @@
+	frame 1, 02
+	frame 2, 02
+	frame 3, 08
+	frame 4, 04
+	frame 3, 04
+	frame 4, 04
+	frame 3, 12
+	frame 2, 02
+	frame 1, 02
+	frame 0, 12
+	frame 5, 04
+	frame 0, 04
+	frame 5, 04
+	frame 0, 04
+	endanim
---
+++ gfx/pokemon/omegadge/anim_idle.asm
@@ -0,0 +1,6 @@
+	frame 0, 04
+	frame 5, 04
+	frame 0, 04
+	frame 5, 04
+	frame 0, 04
+	endanim
